220c232b1a73398b0d9db296a6e33010472336df
ali <alibalaei76@gmail.com>
2021-07-11 23:31:42 +0430

    every thing ready for scraping!

--- scrape.py
+++ scrape.py
@@ -13,16 +13,15 @@ django.setup()
 from scrape.models import *
 
 class Hadithclass():
-    def __init__(self):
-        
-        self.driver = webdriver.Chrome('./chromedriver')
-        base_id = 48113
-        self.source_id = 48113
-        self.base_group_id = 0
-        self.URL = f"https://hadith.inoor.ir/fa/hadith/{base_id}"
-
-        self.hadith_page()
+    def __init__(self):     
+        self.driver = webdriver.Chrome('./chromedriver')   
     
+    def start_scrape(self,id):
+        self.base_id = id
+        self.base_group_id = 0
+        self.URL = f"https://hadith.inoor.ir/fa/hadith/{self.base_id}"
+        self.hadith_page()
+       
     def get_buttons(self):
         self.button_refrence_headers_before = self.driver.find_element_by_xpath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/hadith/section/div[2]/div/mat-tab-group/mat-tab-header/div[1]")
         self.button_refrence_headers_after = self.driver.find_element_by_xpath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/hadith/section/div[2]/div/mat-tab-group/mat-tab-header/div[3]")
@@ -31,6 +30,11 @@ class Hadithclass():
         self.refrence_hedears_div = self.driver.find_element_by_class_name("mat-tab-labels")
         self.len_refrence_headers_divs = len(self.refrence_hedears_div.find_elements_by_class_name("mat-tab-label"))
 
+    def go_to_hadith_by_header(self,i):
+        div = self.driver.find_element_by_id(f"mat-tab-label-0-{i}")
+        span = div.find_element_by_tag_name("span")
+        span.click()  
+        time.sleep(1.5)
     
     def hadith_page(self):
         
@@ -75,11 +79,13 @@ class Hadithclass():
         hadith_tellers_list = h2s[-1].find_element_by_tag_name("span").text.split(",")
         hadith_text = hadith.find_element_by_tag_name("hadith").text
         
+        
         self.hadith_obj , created = Hadith.objects.get_or_create(
                 source_identifier=hadith_identifier
                 )
+        print(f" createad is {created} {hadith_identifier}")
         if not created :
-                print(f"step {i} return true  {self.hadith_obj}")
+                print(f"step {i} {self.hadith_obj} is duplicated")
                 return True
 
         for hadith_teller in hadith_tellers_list :
@@ -107,6 +113,7 @@ class Hadithclass():
         self.hadith_obj.save()
 
         return False
+    
     def get_hadith_translation(self):
         
         translation_header_div = self.driver.find_element_by_id("mat-tab-label-1-0")
@@ -126,7 +133,7 @@ class Hadithclass():
             page = translation_refrence.text.split().pop()
             volume = translation_refrence.text.split().pop(-3)
 
-            print(f" ref name : {name} | vol : {volume} | page : {page} ")
+            #print(f" ref name : {name} | vol : {volume} | page : {page} ")
             translation_text = translation_text.text
 
             TranslationReference.objects.create(
@@ -136,8 +143,6 @@ class Hadithclass():
                 volume = volume , 
                 page = page ,
             )
-
-        
 
     def get_hadith_explaination(self):
         explaination_header_div = self.driver.find_element_by_id("mat-tab-label-1-1")
@@ -163,24 +168,23 @@ class Hadithclass():
                 volume = volume , 
                 page = page ,
             )
-            
-        
+                
     def iterate_in_headers(self):
         step_counter = 0
         for i in range(self.len_refrence_headers_divs):
-            print(f"step {i} ########### source id : {self.source_id}")
-            if i - step_counter > 4 :
+            
+            if i - step_counter > 2 :
                 button_after_class = self.button_refrence_headers_after.get_attribute("class")
                 if not "disabled" in button_after_class:
                     self.button_refrence_headers_after.click()
                 step_counter = i
-            
-            print(f"step {i} ")
+            self.go_to_hadith_by_header(i)
             
             isduplicated = self.get_hadith_data(i)
             if isduplicated :
-                print(f"step {i} continuew occured {self.hadith_obj}")
+                #print(f"step {i} continuew occured {self.hadith_obj}")
                 continue
+            print(f"step {i} ########### source id : {self.hadith_obj}")
             time.sleep(3)
             self.extract_ref(i)
             time.sleep(3)            
@@ -188,8 +192,10 @@ class Hadithclass():
             time.sleep(3)
             self.get_hadith_explaination()
             time.sleep(3)
+            print("")
 
-        self.driver.close()    
+        #self.driver.close()    
 
-
-Hadithclass()
+hadithobj = Hadithclass()
+for id in range(48113 , 500000):
+    hadithobj.start_scrape(id)
